53379465961fc464dd97c3ba8b1381ac91d8ba6c
JAHC-DeV <joseahc2112@gmail.com>
2023-10-11 12:21:00 -0400

    use cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as express from 'express';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -14,7 +15,17 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.use(express.json({ limit: '50mb' }), express.urlencoded({ extended: true, limit: '50mb' }))
+  /*const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // Cambia a la URL de tu cliente
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Habilita el envío de cookies y encabezados de autenticación
+  };*/
+  app.enableCors();
+  app.use(
+    express.json({ limit: '50mb' }),
+    express.urlencoded({ extended: true, limit: '50mb' }),
+  );
   await app.listen(3000);
 }
 bootstrap();
